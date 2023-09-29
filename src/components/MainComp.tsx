718b28a676a899dcb7abb1abb7d1cd031a8906b1
zinx110 <tasdid110@gmail.com>
2023-09-29 21:18:26 +0600

    update

--- src/components/MainComp.tsx
+++ src/components/MainComp.tsx
@@ -13,24 +13,26 @@ const MainComp = ({ data }: any) => {
                 className="object-cover"
             />
             <div className="absolute text-black drop-shadow-md shadow-black w-full h-full flex flex-col justify-between items-center p-5">
-                <p className="font-semibold text-2xl">
+                <p className="font-semibold text-base sm:text-lg md:text-xl lg:text-2xl">
                     I`m Gojo and I got your IP Address Nes
                 </p>
                 <div className=" text-white drop-shadow-md shadow-black w-full  grid grid-cols-2 gap-1 items-center">
-                    <p className="font-semibold text-lg">ip : {data?.ip}</p>
-                    <p className="font-semibold text-lg">
+                    <p className="font-semibold text-sm sm:text-base md:text-lg">
+                        ip : {data?.ip}
+                    </p>
+                    <p className="font-semibold text-sm sm:text-base md:text-lg">
                         country : {data?.country_name}
                     </p>
-                    <p className="font-semibold text-lg">
+                    <p className="font-semibold text-sm sm:text-base md:text-lg">
                         region : {data?.region_name}
                     </p>
-                    <p className="font-semibold text-lg">
+                    <p className="font-semibold text-sm sm:text-base md:text-lg">
                         city : {data?.city_name}
                     </p>
-                    <p className="font-semibold text-lg">
+                    <p className="font-semibold text-sm sm:text-base md:text-lg">
                         latitude : {data?.latitude}
                     </p>
-                    <p className="font-semibold text-lg">
+                    <p className="font-semibold text-sm sm:text-base md:text-lg">
                         longitude : {data?.longitude}
                     </p>
                 </div>
